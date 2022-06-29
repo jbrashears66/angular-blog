bf9eb16c0d846685ec814af67bf997a0d9697c8d
James Brashears <james.brashears@gmail.com>
2022-06-29 14:38:45 -0700

    d

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 
 const routes: Routes = [
   { path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) },
-  { path: '', redirectTo: 'blog', pathMatch: 'full' },
+  { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
   { path: '**', redirectTo: 'blog', pathMatch: 'full' },
 ];
 

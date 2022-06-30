e2d96f96e2926a88d5cfaf23d1731c46f6ea8e5a
James Brashears <james.brashears@gmail.com>
2022-06-29 17:45:18 -0700

    a

--- src/app/blog/blog-list/blog-list.component.ts
+++ src/app/blog/blog-list/blog-list.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import {ScullyRoute, ScullyRoutesService} from "@scullyio/ng-lib";
 
 import {Observable} from "rxjs";
-import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-blog-list',
@@ -11,18 +10,17 @@ import { tap } from 'rxjs';
 })
 export class BlogListComponent implements OnInit {
 
-  links$: Observable<ScullyRoute[]> = this.scully.available$;
-
+ // links$: Observable<ScullyRoute[]> = this.scully.available$;
+  posts: ScullyRoute[] = [];
   constructor(private scully: ScullyRoutesService) { }
 
   ngOnInit(): void {
     this.getPublishedPosts();
   }
   getPublishedPosts() {
-    //this.links$ = this.links$.pipe(tap(eval) => console.log(eval))
-   // this.links$.subscribe((link) => link.filter((link) => link.route.startsWith('/blog'))),console.log(link);
-    //this.links$ = this.links$.pipe(map((links) => links.filter((link) =>
-   // link.route.startsWith('/blog/'))),tap((val) => console.log(val)));
+    this.scully.available$.subscribe(posts => {
+      this.posts = posts.filter(post => post.title);
+    });
   }
 
 
